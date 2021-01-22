c2ab8bfaa2ec733eb9f88a1ac20903fd4be3e9be
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-22 14:44:19 +0200

    Added state array

--- Minesweeper/task/src/minesweeper/Field.java
+++ Minesweeper/task/src/minesweeper/Field.java
@@ -1,5 +1,6 @@
 package minesweeper;
 
+import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Collections;
 import java.util.logging.Logger;
@@ -12,34 +13,38 @@ import static java.util.stream.IntStream.range;
 public class Field {
     private static final Logger log = Logger.getLogger(Field.class.getName());
 
+    private static final int STATE_UNEXPLORED = -1;
+    private static final int STATE_BOMB = -2;
+    private static final int STATE_FREE = 0;
     private static final int[][] OFFSETS =
             new int[][]{{0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}};
 
     public static final int SIZE = 9;
-    public static final int MAX_MINES = SIZE * SIZE - 1;
+    public static final int CELLS = SIZE * SIZE;
     public static final int MIN_MINES = 1;
+    public static final int MAX_MINES = CELLS - 1;
 
-
-    private final BitSet mines = new BitSet(SIZE * SIZE);
-    private final BitSet suggestions = new BitSet(SIZE * SIZE);
+    private final BitSet mines = new BitSet(CELLS);
+    private final BitSet suggestions = new BitSet(CELLS);
     private final int[] numbers;
+    private final int[] state = new int[CELLS];
 
     Field(final int mines) {
+        Arrays.fill(state, STATE_UNEXPLORED);
+
         for (int i = 0; i < mines; i++) {
             this.mines.set(getRandomFreeCell());
         }
 
-        numbers = range(0, SIZE * SIZE).map(this::countMines).toArray();
+        numbers = range(0, CELLS).map(this::countMines).toArray();
         log.config(this.mines::toString);
         suggestions.or(this.mines);
         log.config(this::toString);
         suggestions.clear();
-        //log.config(Arrays.toString(numbers));
     }
 
     private int getRandomFreeCell() {
-        final var freeCells = range(0, SIZE * SIZE)
-                .filter(i -> !mines.get(i)).boxed().collect(toList());
+        final var freeCells = range(0, CELLS).filter(i -> !mines.get(i)).boxed().collect(toList());
         Collections.shuffle(freeCells);
         return freeCells.get(0);
     }
@@ -67,13 +72,26 @@ public class Field {
     @Override
     public String toString() {
         return " │123456789│\n—│—————————│\n"
-                + range(0, SIZE * SIZE).mapToObj(this::getSymbol).collect(joining())
+                + range(0, CELLS).mapToObj(this::getSymbol).collect(joining())
                 + "—│—————————│";
+    }
+
+    private char getState(final int index) {
+        switch (state[index]) {
+            case STATE_UNEXPLORED:
+                return '.';
+            case STATE_BOMB:
+                return '*';
+            case STATE_FREE:
+                return '/';
+            default:
+                return Character.forDigit(state[index], 10);
+        }
     }
 
     private String getSymbol(int index) {
         return (index % SIZE == 0 ? (index / SIZE + 1) + "|" : "")
-                + (suggestions.get(index) ? '*' : mines.get(index) ? '.' : countMines(index))
+                + getState(index)
                 + (index % SIZE == SIZE - 1 ? "|\n" : "");
     }
 
@@ -83,7 +101,6 @@ public class Field {
         final var x = index % SIZE;
         final var y = index / SIZE;
         final var count = stream(OFFSETS).filter(o -> isMine(x + o[0], y + o[1])).count();
-        log.fine(String.format("Index: %d, x = %d, y = %d, Count: %d", index, x, y, count));
         return count == 0 ? '.' : Character.forDigit((int) count, 10);
     }
 
