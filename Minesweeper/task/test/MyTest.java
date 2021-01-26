5a82a3adc6effb4395be1fbeeed130a7dc3da41e
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-26 08:26:31 +0200

    Added the very first test

--- Minesweeper/task/test/MyTest.java
+++ Minesweeper/task/test/MyTest.java
@@ -1,16 +1,18 @@
 import org.hyperskill.hstest.dynamic.DynamicTest;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.testing.TestedProgram;
 
 
 public class MyTest extends StageTest {
     @DynamicTest
-    CheckResult test1() {
+    CheckResult FirstQuestionTest() {
+        final var program = new TestedProgram();
+        final var output = program.start().toLowerCase();
+        if (!output.contains("how many mines")) {
+            return CheckResult.wrong("The program should ask user about number of mines on the field.");
+        }
         return CheckResult.correct();
     }
 
-    @DynamicTest
-    CheckResult test2() {
-        return CheckResult.wrong("Hello, World!");
-    }
 }
