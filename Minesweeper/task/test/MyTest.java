07596df149a793aad7be69ae26cfb95cc3225279
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-01-26 14:14:42 +0200

    Added build.gradle

--- Minesweeper/task/test/MyTest.java
+++ Minesweeper/task/test/MyTest.java
@@ -1,20 +1,34 @@
 import org.hyperskill.hstest.dynamic.DynamicTest;
+import org.hyperskill.hstest.exception.outcomes.WrongAnswer;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testing.TestedProgram;
 
 import java.util.ResourceBundle;
 
+import static java.text.MessageFormat.format;
+
 public class MyTest extends StageTest {
-    //private static final ResourceBundle messages = ResourceBundle.getBundle("messages");
+    private static final ResourceBundle messages = ResourceBundle.getBundle("messages");
+
+    private static void assertContains(
+            final String output,
+            final String lookFor,
+            final String errorMessage,
+            final Object ...args) {
+
+        if (!output.toLowerCase().contains(lookFor)) {
+            final var feedback = format(messages.getString(errorMessage), args)
+                    + "\nExpected to find '" + lookFor + "' in output."
+                    + "\nYou program output:\n" + output;
+            throw new WrongAnswer(feedback);
+        }
+    }
 
     @DynamicTest
     CheckResult FirstQuestionTest() {
         final var program = new TestedProgram();
-        final var output = program.start().toLowerCase();
-        if (!output.contains("how many mines")) {
-            return CheckResult.wrong("The program should ask user about number of mines on the field.");
-        }
+        assertContains(program.start(), "how many mines", "first_question");
         return CheckResult.correct();
     }
 
@@ -25,10 +39,10 @@ public class MyTest extends StageTest {
     CheckResult GameBoardTest(final int minesCount) {
         final var program = new TestedProgram();
         program.start();
-        final var output = program.execute(String.valueOf(minesCount)).toLowerCase();
-        if (!output.contains("set/unset mines marks")) {
-            return CheckResult.wrong("The program should ask user for coordinates and operation");
-        }
+        final var output = program.execute(String.valueOf(minesCount));
+
+        assertContains(output, "set/unset mines marks", "ask_coordinates");
+
         return CheckResult.correct();
     }
 }
